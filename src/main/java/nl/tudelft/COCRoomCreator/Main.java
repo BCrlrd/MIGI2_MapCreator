1d8a58e3d6ffe5582df8e52f6fe3a431f3144cb7
Bram Crielaard <bram@crielaard.co.uk>
2016-06-10 23:18:04 +0200

    Fix warnings

--- src/main/java/nl/tudelft/COCRoomCreator/Main.java
+++ src/main/java/nl/tudelft/COCRoomCreator/Main.java
@@ -3,9 +3,20 @@ package nl.tudelft.COCRoomCreator;
 /**
  * Main class of the Room Creator.
  */
-public class Main  {
+public final class Main  {
 
-	 public static void main(String[] args) {
+	/**
+	 * Private constructor to avoid initialization (for now).
+	 */
+	private Main() { }
+
+	/**
+	 * Main method which will be called on run time.
+	 *
+	 * @param args
+	 * 		run arguments
+     */
+	public static void main(String[] args) {
 		System.out.println("//TODO: Write room creator");
 	}
 }
