07bef13cca2bc3a1774f5947426f3abb12894646
Bram Crielaard <bram@crielaard.co.uk>
2016-06-13 16:26:38 +0200

    Add room toString
    Adds a toString methods for rooms. Also adds a simple test room to the
    main class.

--- src/main/java/nl/tudelft/coccreator/Main.java
+++ src/main/java/nl/tudelft/coccreator/Main.java
@@ -1,10 +1,19 @@
 package nl.tudelft.coccreator;
 
+import nl.tudelft.coccreator.model.PointLight;
+import nl.tudelft.coccreator.model.Tile;
+import nl.tudelft.coccreator.model.entities.Bomb;
+
 public final class Main  {
 
 	private Main() { }
 
 	public static void main(String[] args) {
-		System.out.println("//TODO: Write room creator");
+		Room room = new Room(10, 10);
+		room.setTile(0, 0, Tile.FLOOR);
+		room.setTile(9, 0, Tile.WALL);
+		room.addEntity(new Bomb(2, 3, 1));
+		room.addLight(new PointLight(2, 3, 1, new Colour(10, 2, 4, 1), 5));
+		System.out.println(room.toString());
 	}
 }
--- src/main/java/nl/tudelft/coccreator/Room.java
+++ src/main/java/nl/tudelft/coccreator/Room.java
@@ -25,4 +25,63 @@ public class Room {
         this.tiles = new Tile[width][height];
 		this.comment = "# Default placeholder comment";
     }
+
+	public void setTile(int x, int y, Tile type) {
+		if (x < width && y < height && x >= 0 && y >= 0) {
+			tiles[x][y] = type;
+		}
+	}
+
+	public void addEntity(Entity ent) {
+		entities.add(ent);
+	}
+
+	public void removeEntity(Entity ent) {
+		entities.remove(ent);
+	}
+
+	public void addLight(PointLight light) {
+		lights.add(light);
+	}
+
+	public void removeLight(PointLight light) {
+		lights.remove(light);
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(comment);
+		sb.append("\n");
+		sb.append(width);
+		sb.append(" ");
+		sb.append(height);
+		sb.append(" ");
+		sb.append(entities.size());
+		sb.append(" ");
+		sb.append(lights.size());
+		sb.append("\n");
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (tiles[x][y] != null) {
+					sb.append(tiles[x][y].getRepresentation());
+				} else {
+					sb.append(Tile.EMPTY.getRepresentation());
+				}
+				if (x < width - 1) {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		for (Entity e : entities) {
+			sb.append(e.toString());
+			sb.append("\n");
+		}
+		for (PointLight p : lights) {
+			sb.append(p.toString());
+			sb.append("\n");
+		}
+		sb.setLength(sb.length() - 1);
+		return sb.toString();
+	}
 }
